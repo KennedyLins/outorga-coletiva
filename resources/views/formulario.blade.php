8dd18d5668222182f823a342eefd67a22a0e4811
KennedyLins <kennedy.lins@gmail.com>
2021-07-09 02:41:36 -0300

    ajuste nas validações

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -42,12 +42,12 @@
 
                 <!-- Início do Formulário -->
                 <div class="form-config">
-                <form class="form" action="./cadastro" method="post">   
+                <form name="f1" class="form" action="./cadastro" method="post">   
                 @csrf 
 
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon3">Sistema Hídrico*</span>
-                    <select class="form-select" name="sistema_hidrico">
+                    <select class="form-select" name="sistema_hidrico" required>
                         <option selected></option>
                         <option value="1">Barra do Juá</option>
                     </select>
@@ -55,79 +55,80 @@
 
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon3">Nome do Solicitante*</span>
-                    <input type="text" class="form-control" id="basic-url" name="nome" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="basic-url" name="nome" aria-describedby="basic-addon3" required>
                 </div>
 
                 <div class="input-group mb-3">
                     <span class="input-group-text">E-mail*</span>
-                    <input type="text" class="form-control" name="email" placeholder="exemplo@exemplo.com" >
+                    <input type="email" class="form-control" id="email" name="email"  placeholder="exemplo@exemplo.com" required>
                     <span class="input-group-text"></span>
-                    <input type="text" class="form-control" placeholder="Repita o e-mail" >
+                    <input type="email" class="form-control" id="repita" name="repita" placeholder="Repita o e-mail" required >
                 </div>
 
                 <div class="input-group mb-3">
                     <span class="input-group-text">CPF/CNPJ*</span>
-                    <input type="text" class="form-control" name="cpf_cnpj" placeholder="Apenas números" >
+                    <input type="number" class="form-control" name="cpf_cnpj" placeholder="Apenas números" required>
                     <span class="input-group-text">RG</span>
-                    <input type="text" class="form-control" name="rg" placeholder="Apenas números" >
+                    <input type="number" class="form-control" name="rg" placeholder="Apenas números" >
                 </div>
 
                 <div class="input-group mb-3">
                     <span class="input-group-text">Local da Captação*</span>
-                    <input type="text" class="form-control" name="local_captacao" placeholder="" >
+                    <input type="text" class="form-control" name="local_captacao" placeholder="" required>
                     <span class="input-group-text">Montante/Jusante*</span>
-                    <input type="text" class="form-control" name="montante_jusante" placeholder="" >
+                    <input type="number" class="form-control" name="montante_jusante" placeholder="" required >
                 </div>
 
                 <div class="input-group mb-3">
                     <span class="input-group-text">Lat. (UTM)*</span>
-                    <input type="text" class="form-control" name="latitude" placeholder="Latitude" >
+                    <input type="text" class="form-control" name="latitude" placeholder="Latitude" required>
                     <span class="input-group-text">Long. (UTM)*</span>
-                    <input type="text" class="form-control" name="longitude" placeholder="Longitude" >
+                    <input type="text" class="form-control" name="longitude" placeholder="Longitude" required>
                 </div>
 
                 <div class="input-group">
                     <span class="input-group-text">Descrever tipo de uso*</span>
-                    <textarea class="form-control" aria-label="With textarea" name="tipo_uso" placeholder="tipo, cultura, área, etc."></textarea>
+                    <textarea class="form-control" aria-label="With textarea" name="tipo_uso" placeholder="tipo, cultura, área, etc." required></textarea>
                 </div> <br>
 
-                <label for="basic-url" class="form-label">Volume Mensal Captado (m3)</label>
+                <h5>Volume Mensal Captado (m3)</h5>
+                <label for="basic-url" class="form-label">Digite 0.0 para os meses onde não houve captação</label>
                 <table class="table table-bordered border-primary">
                 <tbody>
                     <tr>
-                    <td>Jan</td>
-                    <td>Fev</td>
-                    <td>Mar</td>
-                    <td>Abr</td>
+                    <td>Jan*</td>
+                    <td>Fev*</td>
+                    <td>Mar*</td>
+                    <td>Abr*</td>
                     </tr>
                     <tr>
-                    <td><input type="text" name="volume_jan" placeholder=""></input></td>
-                    <td><input type="text" name="volume_fev" placeholder=""></input></td>
-                    <td><input type="text" name="volume_mar" placeholder=""></input></td>
-                    <td><input type="text" name="volume_abr" placeholder=""></input></td>
+                    <td><input type="text" name="volume_jan" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_fev" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_mar" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_abr" placeholder="" required></input></td>
                     </tr>
                     <tr>
-                    <td>Mai</td>
-                    <td>Jun</td>
-                    <td>Jul</td>
-                    <td>Ago</td>
+                    <td>Mai*</td>
+                    <td>Jun*/td>
+                    <td>Jul*</td>
+                    <td>Ago*</td>
                     </tr>
                     <tr>
-                    <td><input type="text" name="volume_mai" placeholder=""></input></td>
-                    <td><input type="text" name="volume_jun" placeholder=""></input></td>
-                    <td><input type="text" name="volume_jul" placeholder=""></input></td>
-                    <td><input type="text" name="volume_ago" placeholder=""></input></td>
+                    <td><input type="text" name="volume_mai" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_jun" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_jul" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_ago" placeholder="" required></input></td>
                     </tr>
-                    <td>Set</td>
-                    <td>Out</td>
-                    <td>Nov</td>
-                    <td>Dez</td>
+                    <td>Set*</td>
+                    <td>Out*</td>
+                    <td>Nov*</td>
+                    <td>Dez*</td>
                     </tr>
                     </tr>
-                    <td><input type="text" name="volume_set" placeholder=""></input></td>
-                    <td><input type="text" name="volume_out" placeholder=""></input></td>
-                    <td><input type="text" name="volume_nov" placeholder=""></input></td>
-                    <td><input type="text" name="volume_dez" placeholder=""></input></td>
+                    <td><input type="text" name="volume_set" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_out" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_nov" placeholder="" required></input></td>
+                    <td><input type="text" name="volume_dez" placeholder="" required></input></td>
                     </tr>
                 </tbody>
                 </table>
@@ -136,39 +137,8 @@
                     <span class="input-group-text">Observações</span>
                     <textarea class="form-control" name="observacao" aria-label="With textarea" placeholder=""></textarea>
                 </div>
-
-
-                <!--
-                <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">@</span>
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                    <span class="input-group-text" id="basic-addon2">@example.com</span>
-                </div>
-
-                <label for="basic-url" class="form-label">Your vanity URL</label>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-                </div>
-
-                <div class="input-group mb-3">
-                    <span class="input-group-text">$</span>
-                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                    <span class="input-group-text">.00</span>
-                </div>
-
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-                    <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" placeholder="Server" aria-label="Server">
-                </div>
-        -->
                 
-                <button class="btn-config-02 btn-primary" type="submit">CADASTRAR</button>
+                <button class="btn-config-02 btn-primary" type="submit" onclick="validar()">CADASTRAR</button>
                 </form>
         </div>
 
@@ -179,9 +149,8 @@
 
 
 
-
-
     <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('js/index.js')}}"></script>
     </body>
 </html>
 
